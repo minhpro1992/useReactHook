0957cdda0157a4753ff6455dcaebed49c6035ac2
macbookpro <minh.nguyenvan@axonactive.com>
2023-11-12 22:56:58 +0700

    feat: add swipe slide show component

--- src/ExampleProductDetail.tsx
+++ src/ExampleProductDetail.tsx
@@ -1,4 +1,5 @@
 import { Advertisement } from "./components/advertisement";
+import { Animation } from "./components/animation";
 import { Header } from "./components/header";
 import { Introduction } from "./components/introduction";
 import { Product } from "./components/product";
@@ -8,6 +9,7 @@ export default function ProductDetail() {
     <>
       <div className="items-center mb-4">
         <Advertisement />
+        <Animation />
       </div>
       <div className="md:p-0 container mx-auto my-2 p-4">
         <Header />
